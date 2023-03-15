c9a4eaa4f5e9126521a08b7c685229aa67b4e556
Nancy Ekwere <99981194+NanaKhodes@users.noreply.github.com>
2023-03-15 16:24:44 +0100

    Create Mightyeightball.js
    In this project, we will build a Magic Eight Ball using control flow in JavaScript.
    The user will be able to input a question, then our program will output a random fortune.

---
+++ Mightyeightball.js
@@ -0,0 +1,53 @@
+let userName = 'Jane' ;
+ userName ? console.log(`Hello, ${userName}!`)
+ : console.log(`Hello!`);
+
+ const userQuestion = 'When am i getting marrried?' ; {
+   console.log('When am i getting married?')
+ }
+
+ let randomNumber = Math.floor(Math.random() * 8);
+
+ let eightBall = '' ;
+
+switch (randomNumber) {
+  case (0) :
+   console.log('Next three months');
+   break;
+
+  case (1):
+    console.log('Eleven years from now');
+    break;
+
+  case (2):
+   console.log('Tommorow');
+   break;
+
+  case (3) :
+   console.log('Next month');
+   break;
+
+  case (4) :
+   console.log('Next week');
+    break;
+
+  case (5):
+    console.log('Next thirty years');
+     break;
+
+  case (6):
+    console.log('In the next three years');
+    break;
+
+  case(7):
+    console.log('I really dont know');
+    break;
+
+
+}
+console.log(eightBall);
+
+
+
+
+  
